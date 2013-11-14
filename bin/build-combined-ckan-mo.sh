c0bc719714b5d9688204eb2685daf0139aa26ec1
Vitor Baptista <vitor@vitorbaptista.com>
2013-11-14 17:59:28 -0300

    Translate some ckanext-spatial terms to Spanish

---
+++ bin/build-combined-ckan-mo.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+# Based on:
+# https://github.com/open-data/ckanext-canada/blob/master/bin/build-combined-ckan-mo.sh
+# https://github.com/formwandler/ckanext-odmetheme/master/bin/build-combined-ckan-mo.sh
+
+HERE=`dirname $0`
+
+msgcat --use-first \
+    "$HERE/../ckanext/mx/i18n/es/LC_MESSAGES/ckan.po" \
+    "$HERE/../../../ckan/i18n/es/LC_MESSAGES/ckan.po" \
+    | msgfmt - -o "$HERE/../../../ckan/i18n/es/LC_MESSAGES/ckan.mo"
